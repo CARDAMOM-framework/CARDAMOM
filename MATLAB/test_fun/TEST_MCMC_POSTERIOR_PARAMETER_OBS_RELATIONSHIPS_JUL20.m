5db23ed685f1f491461d721a6e276c298db6f678
aantonyb <abloom@jpl.nasa.gov>
2020-07-27 13:47:59 -0700

    MHMCMC_parameter_obs_relationships

---
+++ MATLAB/test_fun/TEST_MCMC_POSTERIOR_PARAMETER_OBS_RELATIONSHIPS_JUL20.m
@@ -0,0 +1,32 @@
+%Step 1. Define model "C = A*B"
+DATA.obs=10;
+MODEL=@(pars) pars(:,1).*pars(:,2).^2;
+
+%Step 2. Define p(x)p(O|x)
+MLF=@(DATA,pars) -0.5*(MODEL(pars)-DATA.obs).^2 - 0.5*sum((pars - 2).^2);
+
+%Step 3. Ranges for a and b
+PARS.min=[0.1,0.1];
+PARS.max=[10,10];
+PARS.init=[1,1];
+
+%Step 4. run mcmc
+MCO.nout=100000;
+mcmc_output=MHMCMC(DATA,MLF,PARS,MCO);
+
+Aposterior=mcmc_output.PARSOUT(end/2+1:end,1);
+Bposterior=mcmc_output.PARSOUT(end/2+1:end,2);
+Cposterior=MODEL(mcmc_output.PARSOUT(end/2+1:end,:));
+
+%Step 5. dc/da and dc/db
+dcda=(MODEL([Aposterior+Aposterior*0.001,Bposterior]) - Cposterior)./(Aposterior*0.001);
+dcdb=(MODEL([Aposterior,Bposterior+Bposterior*0.001]) - Cposterior)./(Bposterior*0.001);
+
+%Step 6, make plots
+figure(1);clf
+subplot(2,3,1);plot(Aposterior,Cposterior,'.');xlabel('A');ylabel('C');
+subplot(2,3,2);plot(Bposterior,Cposterior,'.');xlabel('B');ylabel('C');
+subplot(2,3,3);plot(Aposterior,Bposterior,'.');xlabel('A');ylabel('B');
+
+subplot(2,3,4);hist(dcda);xlabel('dC/dA');
+subplot(2,3,5);hist(dcdb);xlabel('dC/dB');
