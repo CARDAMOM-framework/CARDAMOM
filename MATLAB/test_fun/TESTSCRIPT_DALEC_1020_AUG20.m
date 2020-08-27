ff362fdd136d7b008896320769d878514834dc12
aantonyb <abloom@jpl.nasa.gov>
2020-08-27 09:49:57 -0700

    BLOOM added 1020 test script

---
+++ MATLAB/test_fun/TESTSCRIPT_DALEC_1020_AUG20.m
@@ -0,0 +1,12 @@
+
+%Loading generic CBF file
+CBF=CARDAMOM_READ_BINARY_FILEFORMAT('CARDAMOM/DATA/CARDAMOM_DATA_DRIVERS_EXAMPLE.cbf');
+
+%Changing ID
+CBF.ID=1020;
+
+%Worth doing during DALEC_1020 modificaitons, just in case, 
+MD=CARDAMOM_MODEL_LIBRARY(1020,[],1);
+
+%Trying out MDF
+CBR=CARDAMOM_RUN_MDF(CBF);
