631db03df694836a767028536f18a5edc28096e4
Norton <alexander.j.norton@jpl.nasa.gov>
2020-09-02 01:22:20 -0700

    Minor changes in parameter ranges for testing purposes

--- C/projects/CARDAMOM_MODELS/DALEC/DALEC_1020/PARS_INFO_1020.c
+++ C/projects/CARDAMOM_MODELS/DALEC/DALEC_1020/PARS_INFO_1020.c
@@ -160,44 +160,44 @@ CARDADATA->parmin[35]=1;
 CARDADATA->parmax[35]=10000;
 
 /*Initial LAI*/
-CARDADATA->parmin[36]=4.0;
+CARDADATA->parmin[36]=1.0;
 CARDADATA->parmax[36]=5.0;
 
 /*Mean temperature at leaf onset (T_phi)*/
 CARDADATA->parmin[37]=23.0;
-CARDADATA->parmax[37]=23.1;
+CARDADATA->parmax[37]=24.0;
 
 /*Spatial range of mean temperature at leaf onset (T_r)*/
-CARDADATA->parmin[38]=0.95;
+CARDADATA->parmin[38]=0.5;
 CARDADATA->parmax[38]=1.0;
 
 /*Averaging period for temperature growth trigger T (time units of model), usually kept constant*/
 CARDADATA->parmin[39]=1.0;
-CARDADATA->parmax[39]=1.0;
+CARDADATA->parmax[39]=2.0;
 
 /*LAI linear growth constant (inverse of model time units; e.g. days-1 or months-1)*/
-CARDADATA->parmin[40]=0.2;
-CARDADATA->parmax[40]=0.21;
+CARDADATA->parmin[40]=0.1;
+CARDADATA->parmax[40]=0.2;
 
 /*Inverse of leaf longevity during senescence period (inverse of model time units; e.g. days-1 or months-1)*/
 CARDADATA->parmin[41]=0.05;
-CARDADATA->parmax[41]=0.051;
+CARDADATA->parmax[41]=0.06;
 
 /*Intrinsic maximum LAI (m^2 m^-2)*/
-CARDADATA->parmin[42]=5.0;
-CARDADATA->parmax[42]=5.1;
+CARDADATA->parmin[42]=1.0;
+CARDADATA->parmax[42]=5.0;
 
-/*Target survival time for LAI under water-deficit conditions (days)*/
+/*Target survival time for LAI under water-deficit conditions (days; or same unit as ET and PAW)*/
 CARDADATA->parmin[43]=0.1;
-CARDADATA->parmax[43]=0.11;
+CARDADATA->parmax[43]=0.5;
 
 /*Mean daylength at leaf shedding (t_c; in units of hours sunlight per day)*/
-CARDADATA->parmin[44]=12.0;
-CARDADATA->parmax[44]=12.1;
+CARDADATA->parmin[44]=11.0;
+CARDADATA->parmax[44]=13.0;
 
 /*Spatial range of mean daylength at leaf shedding (t_r)*/
-CARDADATA->parmin[45]=1.0;
-CARDADATA->parmax[45]=1.01;
+CARDADATA->parmin[45]=0.5;
+CARDADATA->parmax[45]=1.0;
 
 return 0;
 
