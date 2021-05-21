8ba02d66afdc1b60538f4a1a736ab50aab89719c
aantonyb <abloom@jpl.nasa.gov>
2021-05-21 16:53:45 -0700

    AAB_commit

--- MATLAB/test_fun/TEST_CARDAMOM_CHECK_CBF_ERRORS_MAY21.m
+++ MATLAB/test_fun/TEST_CARDAMOM_CHECK_CBF_ERRORS_MAY21.m
@@ -2,7 +2,10 @@
 cbffilename=[getenv('CARDAMOM_DATA_PATH'),'/CARDAMOM_DATA_DRIVERS_EXAMPLE.cbf'];%edited by MS
 
 
+disp('This script is a test script for the CARDAMOM_CHECK_CBF_FOR_ERRORS function');
 CBF=CARDAMOM_READ_BINARY_FILEFORMAT(cbffilename);
+
+
 
 
 %Step 2. Test 
