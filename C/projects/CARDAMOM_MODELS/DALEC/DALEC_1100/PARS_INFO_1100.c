d0da571a9047e09ac42592158c3d58ac028dd1bd
Paul Levine <alkalifly@gmail.com>
2021-07-19 16:21:08 -0700

    Max infilt. comment unit fix

--- C/projects/CARDAMOM_MODELS/DALEC/DALEC_1100/PARS_INFO_1100.c
+++ C/projects/CARDAMOM_MODELS/DALEC/DALEC_1100/PARS_INFO_1100.c
@@ -151,9 +151,9 @@ CARDADATA->parmax[32]=1;
 CARDADATA->parmin[33]=0.0000001;
 CARDADATA->parmax[33]=0.00001;
 
-/*Maximum infiltration (m/month)*/
-CARDADATA->parmin[34]=100;
-CARDADATA->parmax[34]=2000;
+/*Maximum infiltration (mm/day)*/
+CARDADATA->parmin[34]=1;
+CARDADATA->parmax[34]=1e4;
 
 /*PUW pool*/
 CARDADATA->parmin[35]=1;
