28c69234c653befa01d8685f929c2a55e0bafe51
aantonyb <abloom@jpl.nasa.gov>
2021-08-06 16:02:59 -0700

    FIXED_AND_READY

--- MATLAB/cardamom_fun/CARDAMOM_RUN_MDF.m
+++ MATLAB/cardamom_fun/CARDAMOM_RUN_MDF.m
@@ -49,7 +49,7 @@ if nargin<5 | isempty(command_only); command_only=0;end
 if nargin<4 | isempty(compile); compile=1;end
 %CARDAMOM CBF file
 if isstr(CBF)==0;
-    cbffile=['DUMPFILES/CBF',char(ceil(rand(1,20)*25)+96),'temp.cbf'];
+    cbffile=['DUMPFILES/CBF',char(ceil(rand(1,20)*25)+96),'temp.nc.cbf'];
     CARDAMOM_WRITE_NC_CBF_FILE(CBF,cbffile);
     cbftemp=1;
 else
