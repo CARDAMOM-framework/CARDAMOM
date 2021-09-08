e1393679c4401e019d93c5b3a2af95eed9e9b8b1
aantonyb <abloom@jpl.nasa.gov>
2021-09-08 11:17:33 -0700

    inc

--- MATLAB/cardamom_fun/CARDAMOM_INTEGRATION_TESTS.m
+++ MATLAB/cardamom_fun/CARDAMOM_INTEGRATION_TESTS.m
@@ -38,6 +38,39 @@
 
 
 
+
+
+
+
+nccbffilename1100='CARDAMOM/DATA/CARDAMOM_DEMO_DRIVERS.nc.cbf';
+CBF1100=CARDAMOM_READ_NC_CBF_FILE(nccbffilename1100);
+CBF1100.EDC.values=0;
+CBF1100.EWT.values=CBF1100.EWT.values*NaN;
+CBF1100.Mean_Biomass.values=CBF1100.Mean_Biomass.values*NaN;
+CBF1100.Mean_Fire.values=CBF1100.Mean_Fire.values*NaN;
+CBF1100.Mean_LAI.values=CBF1100.Mean_LAI.values*NaN;
+CBF1100.Mean_GPP.values=CBF1100.Mean_GPP.values*NaN;
+CBF1100.PARPRIORS.values=CBF1100.PARPRIORS.values*NaN;
+CBF1100.OTHERPRIORS.values=CBF1100.OTHERPRIORS.values*NaN;
+CBF1100.PARPRIORUNC.values=CBF1100.PARPRIORUNC.values*NaN;
+CBF1100.OTHERPRIORSUNC.values=CBF1100.OTHERPRIORSUNC.values*NaN;
+CBF1100.EDC.values=0;
+
+
+
+%first test is retrieving parameters. Skip only for partial testing
+retrievepars=1;
+if retrievepars==1
+     cbrfilename1100='DUMPFILES/MODEL_ID_1100_EXAMPLE.cbr';
+    CBR=CARDAMOM_RUN_MDF(CBF1100,[],cbrfilename1100);
+end
+
+
+
+
+
+
+
 nccbffilename1100='CARDAMOM/DATA/MODEL_ID_1100_EXAMPLE.nc.cbf';
  cbrfilename1100='CARDAMOM/DATA/MODEL_ID_1100_EXAMPLE.cbr';
  cbrfilename1100ref='CARDAMOM/DATA/MODEL_ID_1100_EXAMPLEref.cbr';
