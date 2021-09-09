47d62706550427f852dcd56058b9618eb4211913
Anthony Bloom <abloom@jpl.nasa.gov>
2021-09-09 09:29:44 -0700

    Update startup_template.m

--- MATLAB/startup_template.m
+++ MATLAB/startup_template.m
@@ -1,8 +1,8 @@
+%******INSTRUCTION********
 %CARDAMOM/MATLAB/startup_template.m is a mock "startup.m" file
 %Step 1. Create or edit existing startup.m file by typing "edit startup.m" in matlab command window 
 %Step 2. Add the following lines in your startup file
-%Step 3. Adapth the paths to make sure these are consistent with your directory system (e.g. depending on where you placed the "CARDAMOM" folder)
-
+%Step 3. Adapth the paths to make sure these are consistent with your directory system (e.g. depending on where you placed the "CARDAMOM" folder).  We recommend using absolute paths, in case the CARDAMOM folder is not in the current matlab directory, for example “/Users/Yourusername/CARDAMOM_2.1.6c/C”
 %*****CARDAMOM C PATH******
 setenv('CARDAMOM_C_PATH','CARDAMOM_2.1.6c/C');
 setenv('CARDAMOM_DATA_PATH','CARDAMOM_2.1.6c/DATA');
