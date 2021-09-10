8edcd328b533d0468b80a4e83c50ce14cfc64f7c
Greg Ross Quetin <gquetin@sh01-ln01.stanford.edu>
2021-09-10 16:38:41 -0700

    Update neutral start condition uncertainty from 1 to 0.05.

--- C/projects/DALEC_CODE/MODEL_LIKELIHOOD_FUNCTIONS/DALEC_LIKELIHOOD_NEUTRALSTART.c
+++ C/projects/DALEC_CODE/MODEL_LIKELIHOOD_FUNCTIONS/DALEC_LIKELIHOOD_NEUTRALSTART.c
@@ -7,13 +7,13 @@ int nodays=D.nodays;
 
 /* Neutral start for NBE */
 double nee_start = 0;
-double nee_start_condition = 0.000001;
+double nee_start_condition = 0.0;
 int years_of_start = 10;
 int days_of_start;
 days_of_start = years_of_start*12;
 
 /*Tolerance on neutrality*/
-double nee_start_tol=1;
+double nee_start_tol=.05; /* No impact from nee_start_tol = 1 */
 
 /*Continuous function to constrain starting years to zero source*/
 /*This constraint serves as a loose bound towards no long term source*/
@@ -29,10 +29,9 @@ for (n=0;n<days_of_start;n++){nee_start=nee_start+D.M_NEE[n];}
 /*normalize by month for means*/
 nee_start=nee_start/days_of_start;
 
-if (nee_start == 0){nee_start=.000001;}
         
 /* calculate the difference between the start and end */
-tot_exp=pow((nee_start_condition-nee_start)/nee_start_tol,2);
+tot_exp=pow(((nee_start_condition-nee_start)/nee_start_tol),2);
 /*tot_exp=pow((log(nee_start_condition/nee_start)/log(nee_start_tol)),2);*/
 
 /*
