e855eff9d918db3cbac9de5cf062763e0bbbd404
Paul Levine <alkalifly@gmail.com>
2021-11-05 22:16:26 -0700

    max_infil priors

--- C/projects/CARDAMOM_MODELS/DALEC/DALEC_1100/PARS_INFO_1100.c
+++ C/projects/CARDAMOM_MODELS/DALEC/DALEC_1100/PARS_INFO_1100.c
@@ -149,8 +149,8 @@ CARDADATA->parmin[P.hydr_cond]=0.0000001;
 CARDADATA->parmax[P.hydr_cond]=0.00001;
 
 /*Maximum infiltration (mm/day)*/
-CARDADATA->parmin[P.max_infil]=1;
-CARDADATA->parmax[P.max_infil]=1e4;
+CARDADATA->parmin[P.max_infil]=0.0005;
+CARDADATA->parmax[P.max_infil]=50;
 
 /*PUW pool*/
 CARDADATA->parmin[P.i_PUW]=1;
