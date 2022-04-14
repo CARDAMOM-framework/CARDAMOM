b27b1e56dd8dfd1b634ba7a81a8379759c3f175c
aantonyb <abloom@jpl.nasa.gov>
2022-04-14 14:14:27 -0700

    inc

---
+++ C/projects/CARDAMOM_MODELS/DALEC/DALEC_ALL/SOIL_TEMP_AND_LIQUID_H2O.c
@@ -0,0 +1,32 @@
+typedef struct {    
+    struct inputs{
+    dry_soil_density;//kg/m3
+    depth;//m
+    soil_moisture;//mm
+    thermal_energy;//Joules
+    }
+    struct outputs{
+           soil_temp;//deg C
+   liquid_h2o_frac;//0-1
+    }
+ }SOIL_TEMP_AND_LIQUID_H2O_STRUCT;
+
+typedef struct{
+
+   
+}SOIL_TEMP_AND_LIQUID_H2O_OUT
+    
+
+//Main function 
+//SOIL_TEMP_AND_LIQUID_H2O_OUT SOIL_TEMP_AND_LIQUID_H2O(SOIL_TEMP_AND_LIQUID_H2O_STRUCT * ST);
+    
+
+  
+    
+    
+    return OUT;
+}
+    
+    
+    
+    
