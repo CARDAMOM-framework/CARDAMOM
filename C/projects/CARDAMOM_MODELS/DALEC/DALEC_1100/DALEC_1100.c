b05a6a96e7def41c4d02ed073a552e96f32397e0
Paul Levine <Paul.A.Levine@jpl.nasa.gov>
2022-07-28 13:24:20 -0700

    hydrofix

--- C/projects/CARDAMOM_MODELS/DALEC/DALEC_1100/DALEC_1100.c
+++ C/projects/CARDAMOM_MODELS/DALEC/DALEC_1100/DALEC_1100.c
@@ -390,13 +390,9 @@ FLUXES[f+F.infil] = infil;
 // Surface runoff (mm/day)
 FLUXES[f+F.q_surf] = (PREC[n] - SNOWFALL[n] + FLUXES[f+F.melt]) - infil;
 
-// Update pools, including infiltration
-POOLS[nxp+S.H2O_PAW] = POOLS[p+S.H2O_PAW] + deltat*infil;
-POOLS[nxp+S.H2O_PUW] = POOLS[p+S.H2O_PUW];
-
 // Volumetric soil moisture from water pools
-double sm_PAW = HYDROFUN_EWT2MOI(POOLS[nxp+S.H2O_PAW],pars[P.PAW_por],pars[P.PAW_z]);
-double sm_PUW = HYDROFUN_EWT2MOI(POOLS[nxp+S.H2O_PUW],pars[P.PUW_por],pars[P.PUW_z]);
+double sm_PAW = HYDROFUN_EWT2MOI(POOLS[p+S.H2O_PAW],pars[P.PAW_por],pars[P.PAW_z]);
+double sm_PUW = HYDROFUN_EWT2MOI(POOLS[p+S.H2O_PUW],pars[P.PUW_por],pars[P.PUW_z]);
 
 // Update PAW SM with infiltration
 //sm_PAW += HYDROFUN_EWT2MOI(infil*deltat,pars[P.PAW_por],pars[P.PAW_z]);
--- C/projects/DALEC_CODE/DALEC_ALL/HYDROLOGY_MODULES/DRAINAGE.c
+++ C/projects/DALEC_CODE/DALEC_ALL/HYDROLOGY_MODULES/DRAINAGE.c
@@ -22,13 +22,21 @@ double sm_field = HYDROFUN_PSI2MOI(psi_field,psi_porosity,b);
 // change in soil moisture (zero if soil moisture is less than field capacity)
 double delta_sm = fmax(sm - sm_field,0);
 
+// check for excessive soil moisture
+double avail_sm = 1 - sm;
+double excess_drainage = 0;
+// excess_sm > 0 when 
+if (delta_sm > avail_sm) {
+  excess_drainage = delta_sm - avail_sm; 
+  delta_sm = avail_sm;}
+
 // potential of layer
 double psi = HYDROFUN_MOI2PSI(sm,psi_porosity,b);
 
   
   //Matlab line Massoud et al.  D= frac_Qexcess * (1- (PARS.psiporosity-min(max(STATES.PSI(t,:),PARS.psifield),PARS.psiporosity))./(PARS.psiporosity-PARS.psifield)).*(SMdelta);
 // calculate drainage (equals zero if change in soil moisture is zero)
-double drainage = delta_sm * Qexcess * (1 - (psi_porosity - fmin(fmax(psi,psi_field),psi_porosity))/(psi_porosity-psi_field));
+double drainage = excess_drainage + delta_sm * Qexcess * (1 - (psi_porosity - fmin(fmax(psi,psi_field),psi_porosity))/(psi_porosity-psi_field));
 
 return drainage;
 
