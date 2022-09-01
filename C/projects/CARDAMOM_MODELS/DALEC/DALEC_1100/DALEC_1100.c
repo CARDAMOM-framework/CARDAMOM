679c353b1deb748023fcb24cb4022bf299cb9c83
Paul Levine <Paul.A.Levine@jpl.nasa.gov>
2022-08-31 23:37:02 -0700

    un-comment

--- C/projects/CARDAMOM_MODELS/DALEC/DALEC_1100/DALEC_1100.c
+++ C/projects/CARDAMOM_MODELS/DALEC/DALEC_1100/DALEC_1100.c
@@ -768,15 +768,15 @@ POOLS[nxp+S.H2O_PUW] = POOLS[p+S.H2O_PUW] + (FLUXES[f+F.paw2puw] - FLUXES[f+F.q_
 
 
 
-// if (POOLS[nxp+S.H2O_PAW]>pars[P.PAW_por]*pars[P.PAW_z]){
-// //Dump excess into PAW Q
-// FLUXES[f+F.q_paw] +=POOLS[nxp+S.H2O_PAW]-PAWmax;
-// POOLS[nxp+S.H2O_PAW]=PAWmax;}
+if (POOLS[nxp+S.H2O_PAW]>pars[P.PAW_por]*pars[P.PAW_z]){
+//Dump excess into PAW Q
+FLUXES[f+F.q_paw] +=(POOLS[nxp+S.H2O_PAW]-PAWmax);
+POOLS[nxp+S.H2O_PAW]=PAWmax;}
 
-// if (POOLS[nxp+S.H2O_PUW]>pars[P.PUW_por]*pars[P.PUW_z]){
-// //Dump excess into PAW Q
-// FLUXES[f+F.q_puw] +=POOLS[nxp+S.H2O_PUW]-PUWmax;
-// POOLS[nxp+S.H2O_PUW]=PUWmax;}
+if (POOLS[nxp+S.H2O_PUW]>pars[P.PUW_por]*pars[P.PUW_z]){
+//Dump excess into PAW Q
+FLUXES[f+F.q_puw] +=(POOLS[nxp+S.H2O_PUW]-PUWmax);
+POOLS[nxp+S.H2O_PUW]=PUWmax;}
 
 
 
