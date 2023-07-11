0bcace4418657b449d248cccf670d7a148e992ea
aantonyb <abloom@jpl.nasa.gov>
2023-07-11 13:25:29 -0700

    inc

--- C/projects/CARDAMOM_MODELS/DALEC/DALEC_1100/DALEC_1100.c
+++ C/projects/CARDAMOM_MODELS/DALEC/DALEC_1100/DALEC_1100.c
@@ -417,6 +417,9 @@ int nopools=((DALEC *)DATA.MODEL)->nopools;
 int nofluxes=((DALEC *)DATA.MODEL)->nofluxes;
 
 
+
+//********MAIN LOOP***********
+
 /*repeating loop for each timestep*/
 for (n=0; n < N_timesteps; n++){
 /*ppol index*/
@@ -740,9 +743,12 @@ TEMPxfer_2to3= POOLS[p+S.D_TEMP_LY3];//In K
 
 
 // Update pools, including ET from LY1
-POOLS[nxp+S.H2O_LY1] = POOLS[p+S.H2O_LY1] + (FLUXES[f+F.infil] - FLUXES[f+F.ly1xly2] - FLUXES[f+F.q_ly1] - FLUXES[f+F.evap] - FLUXES[f+F.transp1])*deltat;
-POOLS[nxp+S.H2O_LY2] = POOLS[p+S.H2O_LY2] + (FLUXES[f+F.ly1xly2] - FLUXES[f+F.ly2xly3] - FLUXES[f+F.q_ly2] - FLUXES[f+F.transp2])*deltat;
-POOLS[nxp+S.H2O_LY3] = POOLS[p+S.H2O_LY3] + (FLUXES[f+F.ly2xly3] - FLUXES[f+F.q_ly3])*deltat;
+    //Ensure all pools greater or equal to zero
+
+POOLS[nxp+S.H2O_LY1] = fmax(POOLS[p+S.H2O_LY1] + (FLUXES[f+F.infil] - FLUXES[f+F.ly1xly2] - FLUXES[f+F.q_ly1] - FLUXES[f+F.evap] - FLUXES[f+F.transp1])*deltat,0);
+POOLS[nxp+S.H2O_LY2] = fmax(POOLS[p+S.H2O_LY2] + (FLUXES[f+F.ly1xly2] - FLUXES[f+F.ly2xly3] - FLUXES[f+F.q_ly2] - FLUXES[f+F.transp2])*deltat,0);
+POOLS[nxp+S.H2O_LY3] = fmax(POOLS[p+S.H2O_LY3] + (FLUXES[f+F.ly2xly3] - FLUXES[f+F.q_ly3])*deltat,0);
+
 
 
 
