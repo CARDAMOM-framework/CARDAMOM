631a10111b2009d55958f0662b91ddf8eb7eb5f4
aantonyb <abloom@jpl.nasa.gov>
2023-07-13 21:27:13 -0700

    not_so_scary_acceleration_correction

--- C/projects/CARDAMOM_MODELS/DALEC/DALEC_1100/DALEC_1100.c
+++ C/projects/CARDAMOM_MODELS/DALEC/DALEC_1100/DALEC_1100.c
@@ -1061,10 +1061,10 @@ FLUXES[f+F.rh_ch4] = (FLUXES[f+F.an_rh_lit]+FLUXES[f+F.an_rh_cwd]+FLUXES[f+F.an_
         POOLS[nxp+S.D_PSI_LY3]=HYDROFUN_MOI2PSI(  POOLS[nxp+S.D_SM_LY3],psi_porosity,pars[P.retention]);
 
 
-    //Nancheck
-    int nnn, nancheck=1;
-    for (nnn=0;nnn<14;nnn++){if ( isfinite(POOLS[nxp+nnn])==false){nancheck=0;}};
-    if (nancheck==0){break;}
+    //Isfinite check for 14 progronstic pools only
+    int nnn, isfinitecheck=1;
+    for (nnn=0;nnn<14;nnn++){if ( isfinite(POOLS[nxp+nnn])==false){isfinitecheck=0;}};
+    if (isfinitecheck==0){break;};
 
 }
 
