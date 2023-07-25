cd26e78055e5598519bf9083267aa7f9aba87d3a
Anthony Bloom <abloom@jpl.nasa.gov>
2023-07-25 10:16:48 -0700

    Update DALEC_1100_NC_INFO.c

--- C/projects/CARDAMOM_MODELS/DALEC/DALEC_1100/DALEC_1100_NC_INFO.c
+++ C/projects/CARDAMOM_MODELS/DALEC/DALEC_1100/DALEC_1100_NC_INFO.c
@@ -3,10 +3,20 @@
 FLUX_INFO.NAME[gpp]='Gross Primary productivity';
 FLUX_INFO.ABBREVIATION[gpp]='GPP';
 FLUX_INFO.UNITS[gpp]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[gpp]='GPP, doesn't include maitenance respiration';
 
 FLUX_INFO.NAME[resp_auto]='Gross Primary productivity';
 FLUX_INFO.ABBREVIATION[resp_auto]='GPP';
 FLUX_INFO.UNITS[resp_auto]='gC/m2/day';
+
+
+
+//Parameter info 
+
+PARS_INFO.NAME[twood]='Turnover rate of wood';
+PARS_INFO.ABBREVIATION[twood]='twood';
+PARS_INFO.UNITS[gpp]='gC/m2/day';
+
 
 ...
 
