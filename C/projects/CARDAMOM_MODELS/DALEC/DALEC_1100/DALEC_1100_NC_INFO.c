06763069641e3a5da95933adb7c4d6bca5907fab
Anthony Bloom <abloom@jpl.nasa.gov>
2023-07-25 10:27:24 -0700

    Update DALEC_1100_NC_INFO.c

--- C/projects/CARDAMOM_MODELS/DALEC/DALEC_1100/DALEC_1100_NC_INFO.c
+++ C/projects/CARDAMOM_MODELS/DALEC/DALEC_1100/DALEC_1100_NC_INFO.c
@@ -5,9 +5,10 @@ FLUX_INFO.ABBREVIATION[F.gpp]='GPP';
 FLUX_INFO.UNITS[F.gpp]='gC/m2/day';
 FLUX_INFO.DESCRIPTION[F.gpp]='GPP, doesn't include maitenance respiration';
 
-FLUX_INFO.NAME[F.resp_auto]='Gross Primary productivity';
-FLUX_INFO.ABBREVIATION[F.resp_auto]='GPP';
+FLUX_INFO.NAME[F.resp_auto]='Total autotrophic respiration';
+FLUX_INFO.ABBREVIATION[F.resp_auto]='Rauto';
 FLUX_INFO.UNITS[F.resp_auto]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[F.gpp]='Includes maintenance respiration and growth respiration';
 
 
 
