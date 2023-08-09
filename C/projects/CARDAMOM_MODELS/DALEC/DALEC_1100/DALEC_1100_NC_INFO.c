ca5616eff65f45483aa4bb1dee5f05dbd67df00a
Gregory R. Quetin <gquetin@gmail.com>
2023-08-09 11:24:26 -0700

    Update DALEC_1100_NC_INFO.c
    Added the first half of the fluxes through melt. Left TBD where uncertain, primarily for descriptions and the units for energy.

--- C/projects/CARDAMOM_MODELS/DALEC/DALEC_1100/DALEC_1100_NC_INFO.c
+++ C/projects/CARDAMOM_MODELS/DALEC/DALEC_1100/DALEC_1100_NC_INFO.c
@@ -1,14 +1,240 @@
 
 
+// Carbon, Water, Energy Fluxes
 FLUX_INFO.NAME[F.gpp]='Gross Primary productivity';
 FLUX_INFO.ABBREVIATION[F.gpp]='GPP';
 FLUX_INFO.UNITS[F.gpp]='gC/m2/day';
-FLUX_INFO.DESCRIPTION[F.gpp]='GPP, doesn't include maitenance respiration';
+FLUX_INFO.DESCRIPTION[F.gpp]='GPP, doesn't include maintenance respiration';
+
+FLUX_INFO.NAME[F.gppnet]='Net Gross Primary Productivity';
+FLUX_INFO.ABBREVIATION[F.gppnet]='gppnet';
+FLUX_INFO.UNITS[F.gppnet]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[F.gppnet]='GPP, including maintenance respiration';
 
 FLUX_INFO.NAME[F.resp_auto]='Total autotrophic respiration';
-FLUX_INFO.ABBREVIATION[F.resp_auto]='Rauto';
+FLUX_INFO.ABBREVIATION[F.resp_auto]='resp_auto';
 FLUX_INFO.UNITS[F.resp_auto]='gC/m2/day';
-FLUX_INFO.DESCRIPTION[F.gpp]='Includes maintenance respiration and growth respiration';
+FLUX_INFO.DESCRIPTION[F.resp_auto]='Includes maintenance respiration and growth respiration';
+
+FLUX_INFO.NAME[F.lab_prod]='Labile production';
+FLUX_INFO.ABBREVIATION[F.lab_prod]='lab_prod';
+FLUX_INFO.UNITS[F.lab_prod]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[F.lab_prod]='TBD';
+
+FLUX_INFO.NAME[F.foliar_prod]='Labile release';
+FLUX_INFO.ABBREVIATION[F.foliar_prod]='foliar_prod';
+FLUX_INFO.UNITS[F.foliar_prod]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[F.foliar_prod]='TBD';
+
+FLUX_INFO.NAME[F.root_prod]='Root production';
+FLUX_INFO.ABBREVIATION[F.root_prod]='root_prod';
+FLUX_INFO.UNITS[F.root_prod]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[F.root_prod]='TBD';
+
+FLUX_INFO.NAME[F.wood_prod]='Wood production';
+FLUX_INFO.ABBREVIATION[F.wood_prod]='wood_prod';
+FLUX_INFO.UNITS[F.wood_prod]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[F.wood_prod]='TBD';
+
+FLUX_INFO.NAME[F.fol2lit]='Foliar decomposition';
+FLUX_INFO.ABBREVIATION[F.fol2lit]='fol2lit';
+FLUX_INFO.UNITS[F.fol2lit]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[F.fol2lit]='TBD';
+
+FLUX_INFO.NAME[F.woo2cwd]='Wood decomposition';
+FLUX_INFO.ABBREVIATION[F.woo2cwd]='woo2cwd';
+FLUX_INFO.UNITS[F.woo2cwd]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[F.woo2cwd]='TBD';
+
+FLUX_INFO.NAME[F.roo2lit]='Root decomposition';
+FLUX_INFO.ABBREVIATION[F.roo2lit]='roo2lit';
+FLUX_INFO.UNITS[F.roo2lit]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[F.roo2lit]='TBD';
+
+FLUX_INFO.NAME[F.cwd2som]='CWD decomposition';
+FLUX_INFO.ABBREVIATION[F.cwd2som]='cwd2som';
+FLUX_INFO.UNITS[F.cwd2som]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[F.cwd2som]='TBD';
+
+FLUX_INFO.NAME[F.lit2som]='Litter decomposition';
+FLUX_INFO.ABBREVIATION[F.lit2som]='lit2som';
+FLUX_INFO.UNITS[F.lit2som]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[F.lit2som]='TBD';
+
+FLUX_INFO.NAME[F.f_total]='Total Fire Flux';
+FLUX_INFO.ABBREVIATION[F.f_total]='f_total';
+FLUX_INFO.UNITS[F.f_total]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[F.f_total]='TBD';
+
+FLUX_INFO.NAME[F.f_lab]='Labile fire loss';
+FLUX_INFO.ABBREVIATION[F.f_lab]='f_lab';
+FLUX_INFO.UNITS[F.f_lab]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[F.f_lab]='TBD';
+
+FLUX_INFO.NAME[F.f_fol]='Foliar fire loss';
+FLUX_INFO.ABBREVIATION[F.f_fol]='f_fol';
+FLUX_INFO.UNITS[F.f_fol]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[F.f_fol]='TBD';
+
+FLUX_INFO.NAME[F.f_roo]='Root fire loss';
+FLUX_INFO.ABBREVIATION[F.f_roo]='f_roo';
+FLUX_INFO.UNITS[F.f_roo]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[F.f_roo]='TBD';
+
+FLUX_INFO.NAME[F.f_woo]='Wood fire loss';
+FLUX_INFO.ABBREVIATION[F.f_woo]='f_woo';
+FLUX_INFO.UNITS[F.f_woo]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[F.f_woo]='TBD';
+
+FLUX_INFO.NAME[F.f_cwd]='CWD fire loss';
+FLUX_INFO.ABBREVIATION[F.f_cwd]='f_cwd';
+FLUX_INFO.UNITS[F.f_cwd]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[F.f_cwd]='TBD';
+
+FLUX_INFO.NAME[F.f_lit]='Litter fire loss';
+FLUX_INFO.ABBREVIATION[F.f_lit]='f_lit';
+FLUX_INFO.UNITS[F.f_lit]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[F.f_lit]='TBD';
+
+FLUX_INFO.NAME[F.f_som]='Soil fire loss';
+FLUX_INFO.ABBREVIATION[F.f_som]='f_som';
+FLUX_INFO.UNITS[F.f_som]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[F.f_som]='TBD';
+
+FLUX_INFO.NAME[F.fx_lab2lit]='Fire labile to litter';
+FLUX_INFO.ABBREVIATION[F.fx_lab2lit]='fx_lab2lit';
+FLUX_INFO.UNITS[F.fx_lab2lit]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[F.fx_lab2lit]='Fire transfer labile to litter';
+
+FLUX_INFO.NAME[F.fx_fol2lit]='Fire foliar to litter';
+FLUX_INFO.ABBREVIATION[F.fx_fol2lit]='fx_fol2lit';
+FLUX_INFO.UNITS[F.fx_fol2lit]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[F.fx_fol2lit]='Fire transfer foliar to litter';
+
+FLUX_INFO.NAME[F.fx_roo2lit]='Fire root to litter';
+FLUX_INFO.ABBREVIATION[F.fx_roo2lit]='fx_roo2lit';
+FLUX_INFO.UNITS[F.fx_roo2lit]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[F.fx_roo2lit]='Fire transfer root to litter';
+
+FLUX_INFO.NAME[F.fx_woo2cwd]='Fire wood to CWD';
+FLUX_INFO.ABBREVIATION[F.fx_woo2cwd]='fx_woo2cwd';
+FLUX_INFO.UNITS[F.fx_woo2cwd]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[F.fx_woo2cwd]='Fire transfer wood to CWD';
+
+FLUX_INFO.NAME[F.fx_cwd2som]='Fire CWD to soil';
+FLUX_INFO.ABBREVIATION[F.fx_cwd2som]='fx_cwd2som';
+FLUX_INFO.UNITS[F.fx_cwd2som]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[F.fx_cwd2som]='Fire transfer CWD to soil';
+
+FLUX_INFO.NAME[F.fx_lit2som]='Fire litter to soil';
+FLUX_INFO.ABBREVIATION[F.fx_lit2som]='fx_lit2som';
+FLUX_INFO.UNITS[F.fx_lit2som]='gC/m2/day';
+FLUX_INFO.DESCRIPTION[F.fx_lit2som]='Fire transfer litter to soil';
+
+FLUX_INFO.NAME[F.q_ly1]='LY1 runoff';
+FLUX_INFO.ABBREVIATION[F.q_ly1]='q_ly1';
+FLUX_INFO.UNITS[F.q_ly1]='kgH20/m2/day';
+FLUX_INFO.DESCRIPTION[F.q_ly1]='LY1 runoff';
+
+FLUX_INFO.NAME[F.q_ly2]='LY2 runoff';
+FLUX_INFO.ABBREVIATION[F.q_ly2]='q_ly2';
+FLUX_INFO.UNITS[F.q_ly2]='kgH20/m2/day';
+FLUX_INFO.DESCRIPTION[F.q_ly2]='LY2 runoff';
+
+FLUX_INFO.NAME[F.ly1xly2]='LY1 to LY2 transfer';
+FLUX_INFO.ABBREVIATION[F.ly1xly2]='ly1xly2';
+FLUX_INFO.UNITS[F.ly1xly2]='kgH20/m2/day';
+FLUX_INFO.DESCRIPTION[F.ly1xly2]='LY1->LY2 transfer';
+
+FLUX_INFO.NAME[F.ly2xly3]='LY2 to LY3 transfer';
+FLUX_INFO.ABBREVIATION[F.ly2xly3]='ly2xly3';
+FLUX_INFO.UNITS[F.ly2xly3]='kgH20/m2/day';
+FLUX_INFO.DESCRIPTION[F.ly2xly3]='LY2->LY3 transfer';
+
+FLUX_INFO.NAME[F.q_ly3]='LY3 runoff';
+FLUX_INFO.ABBREVIATION[F.q_ly3]='q_ly3';
+FLUX_INFO.UNITS[F.q_ly3]='kgH20/m2/day';
+FLUX_INFO.DESCRIPTION[F.q_ly3]='LY3 runoff';
+
+FLUX_INFO.NAME[F.q_surf]='Surface runoff';
+FLUX_INFO.ABBREVIATION[F.q_surf]='q_surf';
+FLUX_INFO.UNITS[F.q_surf]='kgH20/m2/day';
+FLUX_INFO.DESCRIPTION[F.q_surf]='Surface runoff';
+
+FLUX_INFO.NAME[F.infil]='Infiltration';
+FLUX_INFO.ABBREVIATION[F.infil]='infil';
+FLUX_INFO.UNITS[F.infil]='kgH20/m2/day';
+FLUX_INFO.DESCRIPTION[F.infil]='INFILTRATION';
+
+FLUX_INFO.NAME[F.infil_e]='Infiltration IE';
+FLUX_INFO.ABBREVIATION[F.infil_e]='infil_e';
+FLUX_INFO.UNITS[F.infil_e]='TBD';
+FLUX_INFO.DESCRIPTION[F.infil_e]='INFILTRATION IE: temp = weight average of snow melt (0C) + liquid precip (@air temp)';
+
+FLUX_INFO.NAME[F.q_ly1_e]='Q LY1 IE';
+FLUX_INFO.ABBREVIATION[F.q_ly1_e]='q_ly1_e';
+FLUX_INFO.UNITS[F.q_ly1_e]='TBD';
+FLUX_INFO.DESCRIPTION[F.q_ly1_e]='Q LY1 IE: temp = LY1 temp';
+
+FLUX_INFO.NAME[F.q_ly2_e]='Q LY2 IE';
+FLUX_INFO.ABBREVIATION[F.q_ly2_e]='q_ly2_e';
+FLUX_INFO.UNITS[F.q_ly2_e]='TBD';
+FLUX_INFO.DESCRIPTION[F.q_ly2_e]='Q LY2 IE: temp = LY2 temp';
+
+FLUX_INFO.NAME[F.q_ly3_e]='Q LY3 IE';
+FLUX_INFO.ABBREVIATION[F.resq_ly3_ep_auto]='q_ly3_e';
+FLUX_INFO.UNITS[F.q_ly3_e]='TBDy';
+FLUX_INFO.DESCRIPTION[F.q_ly3_e]='Q LY3 IE: temp = LY3 temp';
+
+FLUX_INFO.NAME[F.ly1xly2_e]='LY1 to NDZ IE';
+FLUX_INFO.ABBREVIATION[F.ly1xly2_e]='ly1xly2_e';
+FLUX_INFO.UNITS[F.ly1xly2_e]='TBD';
+FLUX_INFO.DESCRIPTION[F.ly1xly2_e]='LY1 to NDZ transfer IE: temp of donor';
+
+FLUX_INFO.NAME[F.ly2xly3_e]='LY2 to LY3 IE';
+FLUX_INFO.ABBREVIATION[F.ly2xly3_e]='ly2xly3_e';
+FLUX_INFO.UNITS[F.ly2xly3_e]='TBD';
+FLUX_INFO.DESCRIPTION[F.ly2xly3_e]='LY2 to LY3 transfer IE: temp of donor';
+
+FLUX_INFO.NAME[F.evap_e]='Evaporation E';
+FLUX_INFO.ABBREVIATION[F.evap_e]='evap_e';
+FLUX_INFO.UNITS[F.evap_e]='TBD';
+FLUX_INFO.DESCRIPTION[F.evap_e]='See Retano's calculation';
+
+FLUX_INFO.NAME[F.transp1_e]='Transpiration 1 E';
+FLUX_INFO.ABBREVIATION[F.transp1_e]='transp1_e';
+FLUX_INFO.UNITS[F.transp1_e]='TBD';
+FLUX_INFO.DESCRIPTION[F.transp1_e]='See Retano's calculation';
+
+FLUX_INFO.NAME[F.transp2_e]='Transpiration 2 E';
+FLUX_INFO.ABBREVIATION[F.transp2_e]='transp2_e';
+FLUX_INFO.UNITS[F.transp2_e]='TBD';
+FLUX_INFO.DESCRIPTION[F.transp2_e]='See Retano's calculation';
+
+FLUX_INFO.NAME[F.transp1]='Transpiration 1';
+FLUX_INFO.ABBREVIATION[F.transp1]='transp1';
+FLUX_INFO.UNITS[F.transp1]='kgH20/m2/day';
+FLUX_INFO.DESCRIPTION[F.transp1]='Transpiration';
+
+FLUX_INFO.NAME[F.transp2]='Transpiration 2';
+FLUX_INFO.ABBREVIATION[F.transp2]='transp2';
+FLUX_INFO.UNITS[F.transp2]='kgH20/m2/day';
+FLUX_INFO.DESCRIPTION[F.transp2]='Transpiration';
+
+FLUX_INFO.NAME[F.evap]='Evaporation';
+FLUX_INFO.ABBREVIATION[F.evap]='evap';
+FLUX_INFO.UNITS[F.evap]='kgH20/m2/day';
+FLUX_INFO.DESCRIPTION[F.evap]='Evaporation';
+
+FLUX_INFO.NAME[F.snowfall]='Snowfall';
+FLUX_INFO.ABBREVIATION[F.snowfall]='snowfall';
+FLUX_INFO.UNITS[F.snowfall]='kgH20/m2/day';
+FLUX_INFO.DESCRIPTION[F.snowfall]='Snowfall';
+
+FLUX_INFO.NAME[F.melt]='Melt';
+FLUX_INFO.ABBREVIATION[F.melt]='melt';
+FLUX_INFO.UNITS[F.melt]='kgH20/m2/day';
+FLUX_INFO.DESCRIPTION[F.melt]='melt';
 
 
 
