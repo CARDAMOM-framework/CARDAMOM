14c24179ca18b5289580b2ba506a0efddb3f4a05
Alex Patton <alex.j.patton@jpl.nasa.gov>
2024-11-22 11:13:53 -0800

    Fix to attribute length

--- C/projects/CARDAMOM_GENERAL/CARDAMOM_RUN_MODEL.c
+++ C/projects/CARDAMOM_GENERAL/CARDAMOM_RUN_MODEL.c
@@ -195,7 +195,7 @@ for(int i = 0; i < CARDADATA.nofluxes; i++){
 
   }
   //FAILONERROR(nc_def_var(	ncid,ncVarAbbreviation , NC_DOUBLE, 2, fluxes_dems, &(fluxesVarID[i]) ));
-  WARNONERROR(nc_put_att_int	(	ncid,fluxesVarID,ncVarAbbreviation,NC_INT,sizeof(int),&i));
+  WARNONERROR(nc_put_att_int	(	ncid,fluxesVarID,ncVarAbbreviation,NC_INT,1,&i));
 }
 //metadata vars
 FAILONERROR(nc_def_var(	ncid,"FLUX_NAMES" , NC_CHAR, 2, fluxes_meta_dems, &(fluxesNameVarID) ));
@@ -223,7 +223,7 @@ for(int i = 0; i < CARDADATA.nopools; i++){
     printf("ERROR in %s at %d: pool ID %d has no defined ABBREVIATION in it's POOLS_META. Add it to your DALEC_####_NC_INFO.c file! This pool will be called %s until you do!\n", __FILE__, __LINE__,i,ncVarAbbreviation);
 
   }
-  WARNONERROR(nc_put_att_int	(	ncid,poolsVarID,ncVarAbbreviation,NC_INT,sizeof(int),&i));
+  WARNONERROR(nc_put_att_int	(	ncid,poolsVarID,ncVarAbbreviation,NC_INT,1,&i));
 
 
   /*if (poolsInfo.NAME != NULL && poolsInfo.NAME[i] != NULL){
@@ -268,7 +268,7 @@ for(int i = 0; i < CARDADATA.nopars; i++){
     printf("ERROR in %s at %d: paramater ID %d has no defined ABBREVIATION in it's PARS_META. Add it to your DALEC_####_NC_INFO.c file! This paramater will be called %s until you do!\n", __FILE__, __LINE__,i,ncVarAbbreviation );
   }
   
-  WARNONERROR(nc_put_att_int	(	ncid,parsVarID,ncVarAbbreviation,NC_INT,sizeof(int),&i));
+  WARNONERROR(nc_put_att_int	(	ncid,parsVarID,ncVarAbbreviation,NC_INT,1,&i));
   /*
   if (parsInfo.NAME != NULL && parsInfo.NAME[i] != NULL){
     WARNONERROR(nc_put_att_text	(	ncid,parsVarID[i],"Name",strlen(parsInfo.NAME[i]),parsInfo.NAME[i]));
