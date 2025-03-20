84a7ca189680156c2b5b0e44df69ff5c3a82ed29
Anthony Bloom <abloom@jpl.nasa.gov>
2025-03-20 11:39:48 -0700

    Create THERMAL_CONDUCTIVITY.c

---
+++ C/projects/CARDAMOM_MODELS/DALEC/DALEC_ALL/THERMAL_CONDUCTIVITY.c
@@ -0,0 +1,32 @@
+#pragma once
+//***IN-OUT form structure***
+typedef struct {    
+struct{
+    double       porosity;//J/m3/K
+    double       soil_water;//mm (or kg/m2)
+    double        soil_liquid_fraction;
+    double       soil_temp;;} IN;//Joules
+ struct {
+     double TEMP;// Temperature in K
+     double LF;} OUT;// Liquid fraction
+}SOIL_TEMP_AND_LIQUID_FRAC_STRUCT;
+
+//***Main function***
+int SOIL_TEMP_AND_LIQUID_FRAC(SOIL_TEMP_AND_LIQUID_FRAC_STRUCT * S){
+    
+double dry_soil_sh=S->IN.dry_soil_vol_heat_capacity * S->IN.depth;
+double UI3 = ( dry_soil_sh + S->IN.soil_water *DGCM_SPECIFIC_HEAT_ICE)*DGCM_T3 ; 
+double UL3 = UI3 + S->IN.soil_water*DGCM_LATENT_HEAT_FUSION_3; 
+
+if (S->IN.internal_energy<UI3){
+    S->OUT.TEMP = (S->IN.internal_energy/UI3)*DGCM_T3 ;S->OUT.LF = 0;}
+else if (S->IN.internal_energy>UL3){
+        S->OUT.TEMP = (S->IN.internal_energy + S->IN.soil_water*DGCM_SPECIFIC_HEAT_WATER*DGCM_T_LIQUID_H2O_ZERO_ENERGY)/(dry_soil_sh + S->IN.soil_water*DGCM_SPECIFIC_HEAT_WATER) ;
+      S->OUT.LF = 1;}
+else {
+    S->OUT.TEMP = DGCM_TK0C;  
+     S->OUT.LF =(S->IN.internal_energy - UI3) /(S->IN.soil_water*DGCM_LATENT_HEAT_FUSION_3);  
+}
+return 0;
+}
+    
