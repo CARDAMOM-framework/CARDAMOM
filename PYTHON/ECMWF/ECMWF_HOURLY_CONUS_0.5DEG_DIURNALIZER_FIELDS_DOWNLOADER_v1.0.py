ba0724a71e7a466aa6de9d4cb9854db52536e711
Anthony Bloom <abloom@jpl.nasa.gov>
2025-06-03 11:21:56 -0700

    Update ECMWF_HOURLY_CONUS_0.5DEG_DIURNALIZER_FIELDS_DOWNLOADER_v1.0.py

--- PYTHON/ECMWF/ECMWF_HOURLY_CONUS_0.5DEG_DIURNALIZER_FIELDS_DOWNLOADER_v1.0.py
+++ PYTHON/ECMWF/ECMWF_HOURLY_CONUS_0.5DEG_DIURNALIZER_FIELDS_DOWNLOADER_v1.0.py
@@ -1,12 +1,39 @@
-import cdsapi
+import cdsapi;
 
-dataset = "reanalysis-era5-single-levels"
-request = {
+global_area_definition=[-89.75, -179.75, 89.75, 179.75];
+global_grid_definition=["0.5/0.5"];
+data_format="netcdf";
+download_format="unarchived";
+hourly_quantities=["2m_temperature","2m_dewpoint_temperature"];
+monthly_quantities=["total_precipitation","skin_temperature","surface_solar_radiation_downwards","snowfall"];
+
+
+def DOWNLOAD_ECMWF_HOURLY_DRIVERS_FOR_CARDAMOM(m, yr);
+
+  #Step 1. Download all monthly averages by hour
+
+    dataset = "reanalysis-era5-single-levels"
+    request = {
     "product_type": ["reanalysis"],
-    "variable": ["surface_solar_radiation_downwards"],
-    "year": ["2010"],
-    "month": ["01"],
-    "day": ["01"],
+    "variable": [
+        "2m_temperature",
+        "surface_solar_radiation_downwards"
+    ],
+    "year": yr,
+    "month": m,
+    "day": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12",
+        "13", "14", "15",
+        "16", "17", "18",
+        "19", "20", "21",
+        "22", "23", "24",
+        "25", "26", "27",
+        "28", "29", "30",
+        "31"
+    ],
     "time": [
         "00:00", "01:00", "02:00",
         "03:00", "04:00", "05:00",
@@ -20,7 +47,15 @@ request = {
     "data_format": "netcdf",
     "download_format": "unarchived",
     "area": [60, -130, 20, -50]
-}
+    }
 
-client = cdsapi.Client()
-client.retrieve(dataset, request).download()
+    client = cdsapi.Client()
+    client.retrieve(dataset, request).download()
+
+
+
+
+for m in list(range(1, 12)):
+  for yr in list(range(2001, 2024)):
+    DOWNLOAD_ECMWF_MONTHLY_DRIVERS_FOR_CARDAMOM(m, yr);
+
