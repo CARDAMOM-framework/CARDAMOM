c56a9c8069af88a2c9bb1b3b152bb820bbf1c816
Anthony Bloom <abloom@jpl.nasa.gov>
2025-07-08 14:08:46 -0700

    Update HYDROFUN_MOI2PSI_WITH_SALINITY.c

--- C/projects/CARDAMOM_MODELS/DALEC/DALEC_ALL/HYDROLOGY_MODULES/CONVERTERS/HYDROFUN_MOI2PSI_WITH_SALINITY.c
+++ C/projects/CARDAMOM_MODELS/DALEC/DALEC_ALL/HYDROLOGY_MODULES/CONVERTERS/HYDROFUN_MOI2PSI_WITH_SALINITY.c
@@ -3,16 +3,22 @@
 /* with participation of A. Bloom, Eren Bilir and Matthew Worden in testing steps. 
 /* [UCR please add any request for acknowledgment for anyone who uses this function, this would be nice until we can reference a publication or product with DOI]
 /* Convert volumetric soil moisture (m3/m3) to potential (MPa)
-Inputs:
-moi - Soil moisture (m3/m3)
-psi_porosity - porosity potential (MPa)
+
+double HYDROFUN_MOI2PSI_WITH_SALINITY(double moi, double psi_porosity, double b, double temp){
+/*Inputs:
+moi - Soil moisture (units: m3/m3)
+psi_porosity - porosity potential (units: MPa)
 b - retention parameter
+temp - soil water temperature (units: K)
 */
-
-double HYDROFUN_MOI2PSI_WITH_SALINITY(double moi, double psi_porosity, double b){
-
-double psi=psi_porosity*(pow((1/moi),b));
  
+double psi=psi_porosity*(pow((1/moi),b));
+/*UCR team: you can propose code in line below; I'm setting this to zero for now  to get the ball rolling*/
+double psi_pi = - 0.0;
+
+/*summing two psi terms
+double psi_total = psi + psi_pi;
+
 return psi;
 }
 
