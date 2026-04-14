27ac195c082df41e360c487a82437643c190602e
Anthony Bloom <abloom@jpl.nasa.gov>
2026-04-14 16:30:27 -0700

    Update DALEC_EDC_PARAMETER_INEQUALITY.c

--- C/projects/CARDAMOM_MODELS/DALEC/DALEC_EDCs/DALEC_EDC_PARAMETER_INEQUALITY.c
+++ C/projects/CARDAMOM_MODELS/DALEC/DALEC_EDCs/DALEC_EDC_PARAMETER_INEQUALITY.c
@@ -4,20 +4,29 @@
 //***************DALEC_EDC_PARS_INEQUALITY*********
 typedef struct {
     int big_par_index;
-    int small_par_index;}DALEC_EDC_PARAMETER_INEQUALITY_STRUCT;
+    int small_par_index;
+    }DALEC_EDC_PARAMETER_INEQUALITY_STRUCT;
 
 
 //General inequality function
 double DALEC_EDC_PARAMETER_INEQUALITY(DATA * DATA, void * EDCstruct){
    
     double PEDC = 0;
+    double tolerance=1.001;
 
     //Reading by casting void pointer to "DALEC_EDC_PARS_INEQUALITY_STRUCT" format
    DALEC_EDC_PARAMETER_INEQUALITY_STRUCT  E = *(DALEC_EDC_PARAMETER_INEQUALITY_STRUCT * ) EDCstruct;
     //Checking inequality
 
-   
-    if (DATA->M_PARS[E.big_par_index]<DATA->M_PARS[E.small_par_index]){PEDC = -INFINITY;} ;
+    double bigp=DATA->M_PARS[E.big_par_index];
+    double smallp=DATA->M_PARS[E.small_par_index]];
+    double r = bigp/smallp;
+    //assumes parameters are the same sign
+    if (r<tolerance){
+        
+        PEDC = -1e6*(tolerance-r);
+    
+    } ;
     
 
     
