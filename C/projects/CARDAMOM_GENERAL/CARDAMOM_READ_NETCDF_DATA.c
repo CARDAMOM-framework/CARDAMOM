0c5c97f5cf045a55cef0dd9db4f79d0867092d51
ebilir <tebilir@gmail.com>
2026-09-15 15:36:45 -0700

    specifically add dynamic output subsetting (updated 4 files only)

--- C/projects/CARDAMOM_GENERAL/CARDAMOM_READ_NETCDF_DATA.c
+++ C/projects/CARDAMOM_GENERAL/CARDAMOM_READ_NETCDF_DATA.c
@@ -391,6 +391,13 @@ double alpha = asin((sin(pi/180*DATA->LAT)*sin(pi/180*DA)+cos(pi/180*DATA->LAT)*
 printf("Done reading all data");
 
 
+DATA->FLUXES_SUBSET_NAMES = ncdf_read_string_array(ncid, "FLUXES_SUBSET", &DATA->FLUXES_SUBSET_COUNT);
+DATA->POOLS_SUBSET_NAMES = ncdf_read_string_array(ncid, "POOLS_SUBSET", &DATA->POOLS_SUBSET_COUNT);
+DATA->FLUXES_SUBSET_INDICES = NULL;
+DATA->POOLS_SUBSET_INDICES = NULL;
+
+printf("FLUXES_SUBSET_COUNT = %d\n", DATA->FLUXES_SUBSET_COUNT);
+printf("POOLS_SUBSET_COUNT = %d\n", DATA->POOLS_SUBSET_COUNT);
 
 
 MCMCID_STRUCT MCMCID;
@@ -407,6 +414,7 @@ MCMCID_STRUCT MCMCID;
 MCMCID.value = ncdf_read_single_double_var(ncid, "MCMCID");
 MCMCID.nITERATIONS = ncdf_read_int_attr(ncid, "MCMCID","nITERATIONS");
 MCMCID.nPRINT = ncdf_read_int_attr(ncid, "MCMCID","nPRINT");
+MCMCID.nWRITE = ncdf_read_int_attr(ncid, "MCMCID","nWRITE");
 MCMCID.nSAMPLES= ncdf_read_int_attr(ncid, "MCMCID","nSAMPLES");
 MCMCID.nSAMPLES_EDC_SEARCH= ncdf_read_int_attr(ncid, "MCMCID","nSAMPLES_EDC_SEARCH");
 MCMCID.nADAPT= ncdf_read_int_attr(ncid, "MCMCID","nADAPT");
